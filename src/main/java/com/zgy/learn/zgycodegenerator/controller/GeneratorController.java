277cd36712eae59d8e6d2413dd34954e73dd2d7a
zgy <lin_zhaoxi@sohu.com>
2022-06-30 00:40:49 +0800

    🍭页面提示

--- src/main/java/com/zgy/learn/zgycodegenerator/controller/GeneratorController.java
+++ src/main/java/com/zgy/learn/zgycodegenerator/controller/GeneratorController.java
@@ -1,6 +1,9 @@
 package com.zgy.learn.zgycodegenerator.controller;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.json.JSONObject;
 import com.zgy.learn.zgycodegenerator.util.Generator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +13,27 @@ import javax.annotation.Resource;
  * @author zgy
  * @date 2022/6/28
  */
+@Slf4j
 @RestController
 public class GeneratorController {
     @Resource
     private Generator generator;
 
     @GetMapping("generator")
-    public String generator() {
-        generator.generate();
-        return "okay";
+    public JSONObject generator() {
+        JSONObject result = new JSONObject();
+        result.putOpt("code", 1);
+        result.putOpt("message", "error");
+        try {
+            log.info("开始生成项目, 时间是:{}", DateUtil.now());
+            generator.generate();
+            log.info("项目生成完毕, 时间是:{}", DateUtil.now());
+            result.putOpt("code", 0);
+            result.putOpt("message", "success");
+            return result;
+        } catch (Exception e) {
+            return result;
+        }
     }
 
 }
